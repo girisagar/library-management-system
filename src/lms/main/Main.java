2d305e435010e4075ef7fe9a88341506d7431869
Sudeep Bagale <bgsudeep@BG-MBP.local>
2015-08-03 01:11:57 -0500

    Docs added

--- src/lms/main/Main.java
+++ src/lms/main/Main.java
@@ -4,12 +4,9 @@ import javafx.application.Application;
 import lms.ui.LoginController;
 
 public class Main {
-//	private static TextField textMemberId; 
 
 	public static void main(String[] args) {
-		
 		Application.launch(LoginController.class);
-//		Application.launch(LoginForm.class);
-//		Application.launch(MainContainer.class);
+
 	}
 }
